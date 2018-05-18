0e1442859459a0890f69aba84a72bb67c152fde0
Glitch (react-star-wars-search-starting-point) <none>
2018-05-18 16:24:33 -0700

    Completed assignment, with comments.

--- src/components/character-search.js
+++ src/components/character-search.js
@@ -1,46 +1,60 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import Spinner from 'react-spinkit';
-import {searchCharacters} from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import Spinner from "react-spinkit";
+// import { searchCharacters } from "../actions";
+import { searchCharactersRequest, searchCharactersSuccess, searchCharactersError } from "../actions";
+// import createStore from "../store";
+import { search } from "../star-wars";
 
 export class CharacterSearch extends React.Component {
-    renderResults() {
-        if (this.props.loading) {
-            return <Spinner spinnerName="circle" noFadeIn />;
-        }
+  renderResults() {
+    // console.log("this.props: ", this.props);
+    // console.log("createStore.getState() ", createStore.getState());
 
-        if (this.props.error) {
-            return <strong>{this.props.error}</strong>;
-        }
-
-        const characters = this.props.characters.map((character, index) => (
-            <li key={index}>{character}</li>
-        ));
-
-        return <ul className="character-search-results">{characters}</ul>;
+    if (this.props.loading) {
+      return <Spinner spinnerName="circle" noFadeIn />;
     }
 
-    render() {
-        return (
-            <div className="character-search">
-                {/* When this form is submitted you should submit the
+    if (this.props.error) {
+      return <strong>{this.props.error}</strong>;
+    }
+
+    const characters = this.props.characters.map((character, index) => <li key={index}>{character}</li>);
+
+    return <ul className="character-search-results">{characters}</ul>;
+  }
+
+  search(e) {
+    e.preventDefault();
+    // Official solution uses the line below and puts the dispatch etc. inside the actions.js file instead of here. Compare.
+    // this.props.dispatch(searchCharacters(this.input.value));
+    // Then consider putting it in the reducers.js file instead (not that I like that any better). See https://redux.js.org/faq/code-structure#structure-business-logic. Then ponder another solution, such as redux-logic and other approaches in https://medium.com/@jeffbski/where-do-i-put-my-business-logic-in-a-react-redux-application-9253ef91ce1
+    const name = this.input.value;
+    this.props.dispatch(searchCharactersRequest(name));
+    search(name)
+      .then(characters => this.props.dispatch(searchCharactersSuccess(characters)))
+      .catch(error => this.props.dispatch(searchCharactersError(error)));
+  }
+
+  render() {
+    return (
+      <div className="character-search">
+        {/* When this form is submitted you should submit the
                     searchCharacters action */}
-                <form>
-                    <input type="search" ref={input => (this.input = input)} />
-                    <button>Search</button>
-                </form>
-                <ul className="character-search-results">
-                    {this.renderResults()}
-                </ul>
-            </div>
-        );
-    }
+        <form onSubmit={e => this.search(e)}>
+          <input type="search" ref={input => (this.input = input)} />
+          <button>Search</button>
+        </form>
+        <ul className="character-search-results">{this.renderResults()}</ul>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    characters: state.characters,
-    loading: state.loading,
-    error: state.error
+  characters: state.characters,
+  loading: state.loading,
+  error: state.error
 });
 
 export default connect(mapStateToProps)(CharacterSearch);
--- src/star-wars.js
+++ src/star-wars.js
@@ -1,18 +1,23 @@
 function _search(name) {
-    if (Math.random() < 0.25) {
-        return Promise.reject('Something went wrong');
-    }
+  if (Math.random() < 0.25) {
+    return Promise.reject("Something went wrong");
+  }
 
-    return fetch(`https://swapi.co/api/people/?search=${name}`).then(res => {
-        if (!res.ok) {
-            return Promise.reject(res.statusText);
-        }
-        return res.json();
-    }).then(data => data.results.map(character => character.name));
+  if (name.trim() === "") {
+    return Promise.reject("Please enter the name (or partial name) you wish to search for in the box above");
+  }
+  return fetch(`https://swapi.co/api/people/?search=${name}`)
+    .then(res => {
+      if (!res.ok) {
+        return Promise.reject(res.statusText);
+      }
+      return res.json();
+    })
+    .then(data => data.results.map(character => character.name));
 }
 
 export function search(name) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(_search(name)), 500);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(_search(name)), 500);
+  });
 }
